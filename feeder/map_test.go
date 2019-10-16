e834bc97c095ccf3cfbfec5e7ba871736d5746ee
miladrahimi <info@miladrahimi.com>
2019-10-16 18:20:03 +0330

    add test for map

---
+++ feeder/map_test.go
@@ -0,0 +1,18 @@
+package feeder_test
+
+import (
+	"github.com/golobby/config/feeder"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMap_Feed(t *testing.T) {
+	f := feeder.Map{
+		"key": "value",
+	}
+
+	c, err := f.Feed()
+	assert.NoError(t, err)
+
+	assert.Equal(t, "value", c["key"])
+}
