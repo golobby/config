f4217598dec098fdf80ad4ccb468a21464fa7eba
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-10 20:12:15 -0500

    Add the AssignSlice methods to retrieve whole slice elements' value

--- assign_utils.go
+++ assign_utils.go
@@ -1,3 +1,7 @@
+// Copyright 2021 Zhaoping Yu.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package config
 
 import (
@@ -36,6 +40,7 @@ func checkPtr(ptr interface{}) (refPtr, refVal reflect.Value, ok bool) {
 // --- assign method ---
 
 func assign(obj reflect.Value, data interface{}, tag string) int {
+
 	src := reflect.ValueOf(data)
 	if !src.IsValid() {
 		return 0
@@ -56,10 +61,14 @@ func assign(obj reflect.Value, data interface{}, tag string) int {
 	switch obj.Kind() {
 	case reflect.Chan, reflect.Func, reflect.Interface, reflect.UnsafePointer:
 		return 0
+
+	case reflect.Slice:
+		return assign_slice(obj, src, tag)
+
 	case reflect.Map:
 		// TODO
 		return 0
-	case reflect.Array, reflect.Slice:
+	case reflect.Array:
 		// TODO
 		return 0
 	}
@@ -111,7 +120,7 @@ func assign_struct(obj, src reflect.Value, data interface{}, tag string) int {
 
 		fsv := src.MapIndex(reflect.ValueOf(name))
 		if fsv.IsValid() {
-			if fv.Kind() == reflect.Ptr && fv.IsNil() {
+			if needNewPtr(fv) {
 				pv := reflect.New(fv.Type().Elem())
 				if assign(pv, fsv.Interface(), tag) > 0 {
 					fv.Set(pv)
@@ -123,7 +132,7 @@ func assign_struct(obj, src reflect.Value, data interface{}, tag string) int {
 				}
 			}
 		} else if ft.Anonymous {
-			if fv.Kind() == reflect.Ptr && fv.IsNil() {
+			if needNewPtr(fv) {
 				pv := reflect.New(fv.Type().Elem())
 				if ret := assign(pv, data, tag); ret > 0 {
 					fv.Set(pv)
@@ -136,6 +145,51 @@ func assign_struct(obj, src reflect.Value, data interface{}, tag string) int {
 			}
 		}
 	}
+
+	return count
+}
+
+func needNewPtr(fv reflect.Value) bool {
+	switch fv.Kind() {
+	case reflect.Slice:
+		return true
+
+	case reflect.Ptr:
+		return fv.IsNil()
+	}
+	return false
+}
+
+// --- assign_slice ---
+
+func assign_slice(obj, src reflect.Value, tag string) int {
+
+	if src.Kind() != reflect.Slice {
+		return 0
+	}
+
+	srcLen := src.Len()
+	if srcLen == 0 {
+		return 0
+	}
+
+	dst := make([]reflect.Value, 0, srcLen)
+
+	objType := obj.Type().Elem()
+
+	count := 0
+
+	for i := 0; i < srcLen; i++ {
+		sv := src.Index(i)
+		ov := reflect.New(objType)
+
+		if assign(ov, sv.Interface(), tag) > 0 {
+			dst = append(dst, ov.Elem())
+			count++
+		}
+	}
+
+	obj.Set(reflect.Append(obj, dst...))
 
 	return count
 }
