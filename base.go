bc166035e4c147be48a194698c6ebc63a984f867
Zhaoping Yu <yuzhaoping1970@gmail.com>
2021-01-11 13:30:34 -0500

    Rebase to the master

--- base.go
+++ base.go
@@ -3,9 +3,8 @@ package config
 import (
 	"strconv"
 	"strings"
-	"sync"
 
-//	"github.com/golobby/config/env"
+	"github.com/golobby/config/assign"
 )
 
 // Feeder is an interface for config feeders that provide content of a config instance.
@@ -19,18 +18,28 @@ type Options struct {
 	Env    string // Env is the file path that locates the environment file.
 }
 
+// The setter interface is used to solve polymorphism of Set() method using for Reload() method.
+type setter interface {
+	Set(key string, value interface{})
+}
+
 // ConfigBase keeps all the Config instance data.
+// The ConfigBase is NOT goroutine safe.
 type ConfigBase struct {
 	EnvConfig
 	feeders []Feeder               // It keeps all the added feeders
 	items   map[string]interface{} // It keeps all the key/value items (excluding environment ones).
-	sync    sync.RWMutex           // It's responsible for (un)locking the items
+}
+
+// Get the Config's env instance.
+func (c *ConfigBase) Env() *EnvConfig {
+	return &c.EnvConfig
 }
 
 // Feed takes a feeder and feeds the Config instance with it.
 // The built-in feeders are in the feeder subpackage.
 func (c *ConfigBase) Feed(f Feeder) error {
-	err := c.doFeed(f)
+	err := c.doFeed(f, c)
 	if err != nil {
 		return err
 	}
@@ -40,14 +49,14 @@ func (c *ConfigBase) Feed(f Feeder) error {
 	return nil
 }
 
-func (c *ConfigBase) doFeed(f Feeder) error {
+func (c *ConfigBase) doFeed(f Feeder, s setter) error {
 	items, err := f.Feed()
 	if err != nil {
 		return err
 	}
 
 	for k, v := range items {
-		c.Set(k, c.parse(v))
+		s.Set(k, c.parse(v))
 	}
 
 	return nil
@@ -55,8 +64,12 @@ func (c *ConfigBase) doFeed(f Feeder) error {
 
 // Reload reloads all the added feeders and applies new changes.
 func (c *ConfigBase) Reload() error {
+	return c.doReload(c)
+}
+
+func (c *ConfigBase) doReload(s setter) error {
 	for _, f := range c.feeders {
-		if err := c.doFeed(f); err != nil {
+		if err := c.doFeed(f, s); err != nil {
 			return err
 		}
 	}
@@ -67,9 +80,6 @@ func (c *ConfigBase) Reload() error {
 // Set stores the given key/value into the Config instance.
 // It keeps all the changes in memory and won't change the Config files.
 func (c *ConfigBase) Set(key string, value interface{}) {
-	c.sync.Lock()
-	defer c.sync.Unlock()
-
 	if c.items == nil {
 		c.items = map[string]interface{}{}
 	}
@@ -82,9 +92,6 @@ func (c *ConfigBase) Set(key string, value interface{}) {
 // It probably needs to be cast to the related data type.
 // It returns false if there is no value for the given key.
 func (c *ConfigBase) Get(key string) (interface{}, bool) {
-	c.sync.RLock()
-	defer c.sync.RUnlock()
-
 	if v, ok := c.items[key]; ok {
 		return v, true
 	}
@@ -93,12 +100,37 @@ func (c *ConfigBase) Get(key string) (interface{}, bool) {
 		return nil, false
 	}
 
-	return lookupX(c.items, key)
+	return lookup(c.items, key)
 }
 
 // GetAll returns all the configuration items (key/values).
 func (c *ConfigBase) GetAll() map[string]interface{} {
 	return c.items
+}
+
+// Assigns struct fields' value by its field's tag (such as the json tag).
+// @param ptr The pointer of struct's instance to set
+// @param key Specify where to get the struct's value
+// @param tag Specify which struct field's tag name used to retrieve
+// @return The count of fields that been assigned, -1 if struct's value not found by the key
+func (c *ConfigBase) AssignStruct(ptr interface{}, key, tag string) int {
+	if data, found := c.Get(key); found {
+		return assign.AssignStruct(ptr, data, tag)
+	}
+
+	return -1
+}
+
+// Assigns slice elements.
+// @param ptr The pointer of slice instance to appent elements
+// @param key Specify where to get the slice elements's value
+// @param tag If element's type is struct, using the tag name to retrieve struct fields
+// @return The count of elements that been assigned, -1 if slice's value not found by the key
+func (c *ConfigBase) AssignSlice(ptr interface{}, key, tag string) int {
+	if data, found := c.Get(key); found {
+		return assign.AssignSlice(ptr, data, tag)
+	}
+	return -1
 }
 
 // parse replaces the placeholders with environment and OS variables.
@@ -137,20 +169,20 @@ func (c *ConfigBase) parse(value interface{}) interface{} {
 }
 
 // lookup searches for the given key in deep and returns related value.
-func lookupX(collection interface{}, key string) (interface{}, bool) {
+func lookup(collection interface{}, key string) (interface{}, bool) {
 	rest := key
 	key, rest = segmentKey(rest)
 
 	if rest == "" {
-		return findX(collection, key)
+		return find(collection, key)
 	}
 
-	c, ok := digX(collection, key)
+	c, ok := dig(collection, key)
 	if !ok {
 		return nil, false
 	}
 
-	return lookupX(c, rest)
+	return lookup(c, rest)
 }
 
 // segment the key by dot('.'), returns the first segment before dot and the rest.
@@ -171,7 +203,7 @@ func segmentKey(rest string) (string, string) {
 }
 
 // find returns the value of given key in the given 1D collection.
-func findX(collection interface{}, key string) (interface{}, bool) {
+func find(collection interface{}, key string) (interface{}, bool) {
 	switch collection.(type) {
 	case map[string]interface{}:
 		if v, ok := collection.(map[string]interface{})[key]; ok {
@@ -192,7 +224,7 @@ func findX(collection interface{}, key string) (interface{}, bool) {
 }
 
 // dig returns the sub-collection of the given collection by the given key.
-func digX(collection interface{}, key string) (interface{}, bool) {
+func dig(collection interface{}, key string) (interface{}, bool) {
 	if v, ok := collection.(map[string]interface{}); ok {
 		if v, ok := v[key]; ok {
 			return v, true
@@ -207,24 +239,30 @@ func digX(collection interface{}, key string) (interface{}, bool) {
 	return nil, false
 }
 
-// New returns a brand new instance of ConfigBase with the given options.
-func NewBase(ops ...Options) (*ConfigBase, error) {
-	c := &ConfigBase{}
-
+// Initialize the instance of ConfigBase with the given options.
+func (c *ConfigBase) init(ops ...Options) error {
 	for _, op := range ops {
 		if op.Env != "" {
 			err := c.FeedEnv(op.Env)
 			if err != nil {
-				return nil, err
+				return err
 			}
 		}
 
 		if op.Feeder != nil {
 			if err := c.Feed(op.Feeder); err != nil {
-				return nil, err
+				return err
 			}
 		}
 	}
+	return nil
+}
 
-	return c, nil
+// New returns a brand new instance of ConfigBase with the given options.
+func NewBase(ops ...Options) (*ConfigBase, error) {
+	c := &ConfigBase{}
+
+	err := c.init(ops...)
+
+	return c, err
 }
