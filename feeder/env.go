4c26d84e084c66639c769afedf086aa768da8963
amirrezaask <raskarpour@gmail.com>
2021-02-07 05:44:20 +0330

    move all test env files to feeer package, remove env sub-package from code

--- feeder/env.go
+++ feeder/env.go
@@ -1,13 +1,20 @@
 package feeder
 
-import "github.com/golobby/config/env"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 type Env struct {
 	Path string
 }
 
 func (e *Env) Feed() (map[string]interface{}, error) {
-	values, err := env.Load(e.Path)
+	values, err := Load(e.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -16,4 +23,71 @@ func (e *Env) Feed() (map[string]interface{}, error) {
 		m[k] = v
 	}
 	return m, nil
+}
+
+// Load reads the given env file and extracts the variables as a string map
+func Load(filename string) (map[string]string, error) {
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(filepath.Clean(path))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	variables, err := read(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return variables, nil
+}
+
+// read opens the given env file and extracts the variables as a string map
+func read(file io.Reader) (map[string]string, error) {
+	items := map[string]string{}
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		key, value, err := parse(scanner.Text())
+		if err != nil {
+			return nil, err
+		}
+
+		if key != "" {
+			items[key] = value
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return items, nil
+}
+
+// parse extracts the key/value from the given line
+func parse(line string) (string, string, error) {
+	ln := strings.TrimSpace(line)
+
+	if len(ln) == 0 {
+		return "", "", nil
+	}
+
+	if ln[0] == '#' {
+		return "", "", nil
+	}
+
+	s := strings.Index(ln, "=")
+	if s == -1 {
+		return "", "", errors.New("Invalid line: " + ln)
+	}
+
+	k := strings.TrimSpace(ln[:s])
+	v := strings.TrimSpace(ln[s+1:])
+
+	return k, v, nil
 }
