311b7b409457926101a74eea52b449fd2f2cac02
Milad Rahimi <info@miladrahimi.com>
2021-02-11 19:01:30 +0330

    add more tests

--- feeder/json_test.go
+++ feeder/json_test.go
@@ -6,23 +6,21 @@ import (
 	"testing"
 )
 
-func Test_Json_Feed_Not_Existing_File_It_Should_Return_Error(t *testing.T) {
+func TestJson_Feed_With_Invalid_JSON_Path_It_Should_Fail(t *testing.T) {
 	j := feeder.Json{Path: "/404.json"}
 
 	_, err := j.Feed()
-
 	assert.Error(t, err)
 }
 
-func Test_Json_Feed_Invalid_JSON_It_Should_Return_Error(t *testing.T) {
+func TestJson_Feed_With_Invalid_JSON_File_It_Should_Fail(t *testing.T) {
 	j := feeder.Json{Path: "test/invalid.json"}
 
 	_, err := j.Feed()
-
 	assert.Error(t, err)
 }
 
-func Test_Json_Feed_Sample1(t *testing.T) {
+func TestJson_Feed_With_Sample_JSON_File(t *testing.T) {
 	j := feeder.Json{Path: "test/config.json"}
 
 	m, err := j.Feed()
@@ -30,4 +28,5 @@ func Test_Json_Feed_Sample1(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "MyAppUsingConfig", m["name"])
 	assert.Equal(t, 3.14, m["version"])
+	assert.Equal(t, "Milad Rahimi", m["users"].([]interface{})[0].(map[string]interface{})["name"])
 }
