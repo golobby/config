0cdacf353d32d36554982e76d91703685ea0345d
Milad Rahimi <info@miladrahimi.com>
2021-02-11 13:56:26 +0330

    add test for os fetch when env is empty

--- feeder/env_test.go
+++ feeder/env_test.go
@@ -1,6 +1,7 @@
 package feeder
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,13 @@ import (
 
 func TestEnv_Feed_With_Invalid_File_Path_It_Should_Fail(t *testing.T) {
 	e := Env{Path: "test/.404"}
+
+	_, err := e.Feed()
+	assert.Error(t, err)
+}
+
+func TestEnv_Feed_With_Empty_File_Path_It_Should_Fail(t *testing.T) {
+	e := Env{}
 
 	_, err := e.Feed()
 	assert.Error(t, err)
@@ -47,4 +55,15 @@ func TestEnv_Feed_It_Should_Read_The_Sample_Env_File(t *testing.T) {
 	assert.Equal(t, "true", items["debug"])
 	assert.Equal(t, "#VALUE!", items["not.comment"])
 	assert.Equal(t, "", items["name"])
+}
+
+func TestEnv_Feed_It_Should_Use_OS_When_Key_Is_Empty_In_Env_File(t *testing.T) {
+	_ = os.Setenv("APP_NAME", "FROM_OS")
+
+	e := Env{Path: "test/.env"}
+
+	items, err := e.Feed()
+	assert.NoError(t, err)
+
+	assert.Equal(t, "FROM_OS", items["app.name"])
 }
