b5370cf916a76c89b3f87a30774159b7063a3965
Milad Rahimi <realmiladrahimi@gmail.com>
2022-06-19 11:15:20 +0430

    make add functions variadic

--- config.go
+++ config.go
@@ -3,81 +3,85 @@
 package config
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
+    "fmt"
+    "os"
+    "os/signal"
+    "syscall"
 )
 
 // Feeder is an interface for configuration Feeders that provide configuration data.
 type Feeder interface {
-	// Feed gets a struct and feeds it using configuration data.
-	Feed(structure interface{}) error
+    // Feed gets a struct and feeds it using configuration data.
+    Feed(structure interface{}) error
 }
 
 // Config is the configuration manager.
 // To use the package facilities, there should be at least one instance of it.
 // It holds the configuration feeders and structs.
 type Config struct {
-	Feeders []Feeder      // Feeders is the list of feeders that provides configuration data.
-	Structs []interface{} // Structs is the list of structs that holds the configuration data.
+    Feeders []Feeder      // Feeders is the list of feeders that provides configuration data.
+    Structs []interface{} // Structs is the list of structs that holds the configuration data.
 }
 
 // New creates a brand new instance of Config to use the package facilities.
 func New() *Config {
-	return &Config{}
+    return &Config{}
 }
 
-// AddFeeder adds a feeder that provides configuration data.
-func (c *Config) AddFeeder(f Feeder) *Config {
-	c.Feeders = append(c.Feeders, f)
-	return c
+// AddFeeder adds one or more feeders that provides configuration data.
+func (c *Config) AddFeeder(fs ...Feeder) *Config {
+    for _, f := range fs {
+        c.Feeders = append(c.Feeders, f)
+    }
+    return c
 }
 
-// AddStruct adds a struct that holds the configuration data.
-func (c *Config) AddStruct(s interface{}) *Config {
-	c.Structs = append(c.Structs, s)
-	return c
+// AddStruct adds one or more structs that holds the configuration data.
+func (c *Config) AddStruct(ss ...interface{}) *Config {
+    for _, s := range ss {
+        c.Structs = append(c.Structs, s)
+    }
+    return c
 }
 
 // Feed binds configuration data from added feeders to the added structs.
 func (c *Config) Feed() error {
-	for _, s := range c.Structs {
-		for _, f := range c.Feeders {
-			if err := c.feedStruct(f, s); err != nil {
-				return err
-			}
-		}
-	}
+    for _, s := range c.Structs {
+        for _, f := range c.Feeders {
+            if err := c.feedStruct(f, s); err != nil {
+                return err
+            }
+        }
+    }
 
-	return nil
+    return nil
 }
 
 // SetupListener adds an OS signal listener to the Config instance.
 // The listener listens to the `SIGHUP` signal and refreshes the Config instance.
 // It would call the provided fallback if the refresh process failed.
 func (c *Config) SetupListener(fallback func(err error)) *Config {
-	s := make(chan os.Signal, 1)
+    s := make(chan os.Signal, 1)
 
-	signal.Notify(s, syscall.SIGHUP)
+    signal.Notify(s, syscall.SIGHUP)
 
-	go func() {
-		for {
-			<-s
-			if err := c.Feed(); err != nil {
-				fallback(err)
-			}
-		}
-	}()
+    go func() {
+        for {
+            <-s
+            if err := c.Feed(); err != nil {
+                fallback(err)
+            }
+        }
+    }()
 
-	return c
+    return c
 }
 
 // feedStruct feeds a struct using given feeder.
 func (c *Config) feedStruct(f Feeder, s interface{}) error {
-	if err := f.Feed(s); err != nil {
-		return fmt.Errorf("config: failed to feed struct; err %v", err)
-	}
+    if err := f.Feed(s); err != nil {
+        return fmt.Errorf("config: failed to feed struct; err %v", err)
+    }
 
-	return nil
+    return nil
 }
--- config_test.go
+++ config_test.go
@@ -1,116 +1,148 @@
 package config_test
 
 import (
-	"github.com/golobby/config/v3"
-	"github.com/golobby/config/v3/pkg/feeder"
-	"github.com/stretchr/testify/assert"
-	"os"
-	"syscall"
-	"testing"
-	"time"
+    "github.com/golobby/config/v3"
+    "github.com/golobby/config/v3/pkg/feeder"
+    "github.com/stretchr/testify/assert"
+    "os"
+    "syscall"
+    "testing"
+    "time"
 )
 
 func TestFeed(t *testing.T) {
-	c := &struct{}{}
-	err := config.New().AddFeeder(feeder.Env{}).AddStruct(c).Feed()
-	assert.NoError(t, err)
+    c := &struct{}{}
+    err := config.New().AddFeeder(feeder.Env{}).AddStruct(c).Feed()
+    assert.NoError(t, err)
 }
 
 func TestFeed_With_Invalid_File_It_Should_Fail(t *testing.T) {
-	s := struct{}{}
-	c := config.New()
-	c.AddFeeder(feeder.Json{})
-	c.AddStruct(&s)
-	err := c.Feed()
-	assert.Error(t, err)
+    s := struct{}{}
+    c := config.New()
+    c.AddFeeder(feeder.Json{})
+    c.AddStruct(&s)
+    err := c.Feed()
+    assert.Error(t, err)
 }
 
-func TestFeed_WithMultiple_Feeders(t *testing.T) {
-	_ = os.Setenv("PRODUCTION", "1")
-	_ = os.Setenv("APP_PORT", "6969")
+func TestFeed_WithMultiple_Feeders_With_Multiple_Calls(t *testing.T) {
+    _ = os.Setenv("PRODUCTION", "1")
+    _ = os.Setenv("APP_PORT", "6969")
 
-	c := &struct {
-		App struct {
-			Name string `env:"APP_NAME"`
-			Port int    `env:"APP_PORT"`
-		}
-		Debug      bool     `env:"DEBUG"`
-		Production bool     `env:"PRODUCTION"`
-		Pi         float64  `env:"PI"`
-		IPs        []string `env:"IPS"`
-		IDs        []int16  `env:"IDS"`
-	}{}
+    c := &struct {
+        App struct {
+            Name string `env:"APP_NAME"`
+            Port int    `env:"APP_PORT"`
+        }
+        Debug      bool     `env:"DEBUG"`
+        Production bool     `env:"PRODUCTION"`
+        Pi         float64  `env:"PI"`
+        IPs        []string `env:"IPS"`
+        IDs        []int16  `env:"IDS"`
+    }{}
 
-	f1 := feeder.Json{Path: "assets/sample1.json"}
-	f2 := feeder.DotEnv{Path: "assets/.env.sample2"}
-	f3 := feeder.Env{}
+    f1 := feeder.Json{Path: "assets/sample1.json"}
+    f2 := feeder.DotEnv{Path: "assets/.env.sample2"}
+    f3 := feeder.Env{}
 
-	err := config.New().AddFeeder(f1).AddFeeder(f2).AddFeeder(f3).AddStruct(c).Feed()
-	assert.NoError(t, err)
+    err := config.New().AddFeeder(f1).AddFeeder(f2).AddFeeder(f3).AddStruct(c).Feed()
+    assert.NoError(t, err)
 
-	assert.Equal(t, "Blog", c.App.Name)
-	assert.Equal(t, 6969, c.App.Port)
-	assert.Equal(t, false, c.Debug)
-	assert.Equal(t, true, c.Production)
-	assert.Equal(t, 3.14, c.Pi)
-	assert.Equal(t, []string{"192.168.0.1", "192.168.0.2"}, c.IPs)
-	assert.Equal(t, []int16{10, 11, 12, 13}, c.IDs)
+    assert.Equal(t, "Blog", c.App.Name)
+    assert.Equal(t, 6969, c.App.Port)
+    assert.Equal(t, false, c.Debug)
+    assert.Equal(t, true, c.Production)
+    assert.Equal(t, 3.14, c.Pi)
+    assert.Equal(t, []string{"192.168.0.1", "192.168.0.2"}, c.IPs)
+    assert.Equal(t, []int16{10, 11, 12, 13}, c.IDs)
+}
+
+func TestFeed_WithMultiple_Feeders_With_A_Single_Call(t *testing.T) {
+    _ = os.Setenv("PRODUCTION", "1")
+    _ = os.Setenv("APP_PORT", "6969")
+
+    c := &struct {
+        App struct {
+            Name string `env:"APP_NAME"`
+            Port int    `env:"APP_PORT"`
+        }
+        Debug      bool     `env:"DEBUG"`
+        Production bool     `env:"PRODUCTION"`
+        Pi         float64  `env:"PI"`
+        IPs        []string `env:"IPS"`
+        IDs        []int16  `env:"IDS"`
+    }{}
+
+    f1 := feeder.Json{Path: "assets/sample1.json"}
+    f2 := feeder.DotEnv{Path: "assets/.env.sample2"}
+    f3 := feeder.Env{}
+
+    err := config.New().AddFeeder(f1, f2, f3).AddStruct(c).Feed()
+    assert.NoError(t, err)
+
+    assert.Equal(t, "Blog", c.App.Name)
+    assert.Equal(t, 6969, c.App.Port)
+    assert.Equal(t, false, c.Debug)
+    assert.Equal(t, true, c.Production)
+    assert.Equal(t, 3.14, c.Pi)
+    assert.Equal(t, []string{"192.168.0.1", "192.168.0.2"}, c.IPs)
+    assert.Equal(t, []int16{10, 11, 12, 13}, c.IDs)
 }
 
 func TestConfig_Feed_For_Refreshing(t *testing.T) {
-	_ = os.Setenv("NAME", "One")
+    _ = os.Setenv("NAME", "One")
 
-	s := &struct {
-		Name string `env:"NAME"`
-	}{}
+    s := &struct {
+        Name string `env:"NAME"`
+    }{}
 
-	c := config.New().AddFeeder(feeder.Env{}).AddStruct(s)
-	err := c.Feed()
-	assert.NoError(t, err)
+    c := config.New().AddFeeder(feeder.Env{}).AddStruct(s)
+    err := c.Feed()
+    assert.NoError(t, err)
 
-	assert.Equal(t, "One", s.Name)
+    assert.Equal(t, "One", s.Name)
 
-	_ = os.Setenv("NAME", "Two")
+    _ = os.Setenv("NAME", "Two")
 
-	err = c.Feed()
-	assert.NoError(t, err)
+    err = c.Feed()
+    assert.NoError(t, err)
 
-	assert.Equal(t, "Two", s.Name)
+    assert.Equal(t, "Two", s.Name)
 }
 
 func TestConfig_SetupListener(t *testing.T) {
-	_ = os.Setenv("PI", "3.14")
+    _ = os.Setenv("PI", "3.14")
 
-	s := &struct {
-		Pi float64 `env:"PI"`
-	}{}
+    s := &struct {
+        Pi float64 `env:"PI"`
+    }{}
 
-	fallbackTested := false
-	c := config.New().AddFeeder(feeder.Env{}).AddStruct(s).SetupListener(func(err error) {
-		fallbackTested = true
-	})
+    fallbackTested := false
+    c := config.New().AddFeeder(feeder.Env{}).AddStruct(s).SetupListener(func(err error) {
+        fallbackTested = true
+    })
 
-	err := c.Feed()
-	assert.NoError(t, err)
+    err := c.Feed()
+    assert.NoError(t, err)
 
-	assert.Equal(t, 3.14, s.Pi)
+    assert.Equal(t, 3.14, s.Pi)
 
-	_ = os.Setenv("PI", "3.666")
+    _ = os.Setenv("PI", "3.666")
 
-	err = syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
-	assert.NoError(t, err)
+    err = syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
+    assert.NoError(t, err)
 
-	time.Sleep(10 * time.Millisecond)
+    time.Sleep(10 * time.Millisecond)
 
-	assert.Equal(t, 3.666, s.Pi)
+    assert.Equal(t, 3.666, s.Pi)
 
-	_ = os.Setenv("PI", "INVALID!")
+    _ = os.Setenv("PI", "INVALID!")
 
-	err = syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
-	assert.NoError(t, err)
+    err = syscall.Kill(syscall.Getpid(), syscall.SIGHUP)
+    assert.NoError(t, err)
 
-	time.Sleep(10 * time.Millisecond)
+    time.Sleep(10 * time.Millisecond)
 
-	assert.Equal(t, true, fallbackTested)
-	assert.Equal(t, 3.666, s.Pi)
+    assert.Equal(t, true, fallbackTested)
+    assert.Equal(t, 3.666, s.Pi)
 }
